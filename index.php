fdf68f4210204c77be6582f90e41468259057f08
Alex Tikhonov <humsterman86@gmail.com>
2014-05-05 13:34:54 +0300

    Release v.1 fixes

--- index.php
+++ index.php
@@ -3,31 +3,15 @@
 include('config.php');
 require_once 'myurl.php';
 
-$nurl = new myurl();
-$postUrl = $_POST['url'];
 $getUrl = $_GET['url'];
 
 if (!empty($getUrl)) {
-    $nurl->redirect($getUrl);
-    $nurl->addHint($getUrl);
+    $redirect = new existingUrl($getUrl);
 }
 
 if ($_POST['url']) {
-
-    $query = mysql_query('SELECT url_short FROM urls WHERE url_link ="'.$postUrl.'"');
-
-    if(mysql_num_rows($query) == 0)
-            {
-            $short = $nurl->shortGeneration();
-            $nurl->insertNewUrl($postUrl, $short, $_SERVER['REMOTE_ADDR']);
-            $nurl->redirectNewLink($short);
-            }
-    else
-            {
-            $sql_url = mysql_fetch_assoc($query);
-            $short_url = $sql_url['url_short'];
-            $nurl->redirectNewLink($short_url);
-            }
+    $postUrl = $_POST['url'];
+    $do = new newUrl($postUrl);
 }
  require('template/template.php'); 
 ?>
--- myurl.php
+++ myurl.php
@@ -1,12 +1,45 @@
 <?php
-class myurl {
-    public function __construct() {
+class existingUrl {
+    public $link;
+    public function __construct($url) {
+        $this->link = $url;
+        $this->redirect($url);
     }
     function redirect ($link) {
 	$redirect = mysql_fetch_assoc(mysql_query("SELECT url_link FROM urls WHERE url_short = '".addslashes($link)."'"));
 	$redirect = "http://".str_replace("http://","",$redirect['url_link']);
 	header('HTTP/1.1 301 Moved Permanently');  
 	header("Location: ".$redirect);  
+    } 
+}
+
+class newUrl {
+    public $link;
+    public $short;
+    public function __construct($url) {
+        $this->link = $url;
+        $this->checkUrl();
+    }    
+
+    function checkUrl() {
+        $query = mysql_query('SELECT url_short FROM urls WHERE url_link ="'.$this->link.'"');
+        if(mysql_num_rows($query) == 0)
+            {
+            $this->newRecord();
+            }
+    else
+            {
+            $sql_url = mysql_fetch_assoc($query);
+            $short_url = $sql_url['url_short'];
+            $this->redirectNewLink($short_url);
+            }
+
+    }
+    
+    function newRecord() {
+        $short = $this->shortGeneration();
+        $this->insertNewUrl($this->link, $short);
+        $this->redirectNewLink($short);
     }
     
     function shortGeneration() {
@@ -14,12 +47,12 @@ class myurl {
         return $short;
     }
 
-    function insertNewUrl($url, $short_url, $server) {
+    function insertNewUrl($url, $short_url) {
         mysql_query("INSERT INTO urls (url_link, url_short, url_ip, url_date) VALUES
 	(
             '".addslashes($url)."',
             '".$short_url."',
-            '".$server."',
+            '".$_SERVER['REMOTE_ADDR']."',
             '".time()."'
 	)
         ") or die (mysql_error());
