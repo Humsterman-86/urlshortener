e9c0f437751b7984f4fd79cb6c01d5d15c58add1
Alex Tikhonov <humsterman86@gmail.com>
2014-05-05 14:58:45 +0300

    Installation script

--- install.php
+++ install.php
@@ -1,5 +1,6 @@
 <?php
 include('config.php');
+echo "Createing main table...";
 //create the urls table if it's not already there:
 mysql_query("CREATE TABLE IF NOT EXISTS `urls` (
   `url_id` int(11) NOT NULL auto_increment,
@@ -10,7 +11,8 @@ mysql_query("CREATE TABLE IF NOT EXISTS `urls` (
   `url_hits` int(11) default '0',
   PRIMARY KEY  (`url_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;");
-
+echo "Main table created!";
+echo "Createing stats table";
 mysql_query("CREATE TABLE IF NOT EXISTS `url_stats` (
   `log_id` int(11) NOT NULL auto_increment,
   `hit_url` varchar(255) NOT NULL,
@@ -18,4 +20,6 @@ mysql_query("CREATE TABLE IF NOT EXISTS `url_stats` (
   `hit_date` int(10) default NULL,  
 PRIMARY KEY  (`log_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;");
+echo "Stats table created!";
+echo "Installation complited! Please, remove file 'install.php' from the root directory";
 ?>
