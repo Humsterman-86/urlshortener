32e8debc8491e529d12a0878769395c02b7d70c2
Alex Tikhonov <humsterman86@gmail.com>
2014-05-05 09:33:45 +0300

    New

--- index.php
+++ index.php
@@ -1,44 +1,31 @@
 <?php
 
 include('config.php');
+require_once 'myurl.php';
 
+$url = new myurl();
 //redirect to real link if URL is set
 if (!empty($_REQUEST['url'])) {
-	$redirect = mysql_fetch_assoc(mysql_query("SELECT url_link FROM urls WHERE url_short = '".addslashes($_GET['url'])."'"));
-	$redirect = "http://".str_replace("http://","",$redirect[url_link]);
-	header('HTTP/1.1 301 Moved Permanently');  
-	header("Location: ".$redirect);  
+    $url->redirect($_REQUEST['url']);
 }
 
+var_dump($_REQUEST);
 //insert new url
 if ($_POST['url']) {
-    $short = substr(str_shuffle('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'), 0, 5);
+    $short = $url->shortGeneration();
+
     $query = mysql_query('SELECT url_short FROM urls WHERE url_link ="'.$_POST['url'].'"');
-     
     if($query == FALSE)
             {
-		mysql_query("INSERT INTO urls (url_link, url_short, url_ip, url_date) VALUES
-			(
-				'".addslashes($_POST['url'])."',
-				'".$short."',
-				'".$_SERVER['REMOTE_ADDR']."',
-				'".time()."'
-			)
-		");
-	    $redirect = "?s=$short";
-	    header('Location: '.$redirect); die;
-
-            $banavtorip = 0;
+            $newUrl = $url->insertNewUrl($_POST['url']);
+            $url->redirectNewLink($newUrl);
             }
     else
             {
             $sql_url = mysql_fetch_assoc($query);
             $short_url = $sql_url['url_short'];
-	    $redirect = "?s=$short_url";
-	    header('Location: '.$redirect); die;
-
+            $url->redirectNewLink($short_url);
             }
-
 }
  require('template/template.php'); 
 ?>
---
+++ myurl.php
@@ -0,0 +1,34 @@
+<?php
+class myurl {
+    public function __construct() {
+    }
+    function redirect ($link) {
+	$redirect = mysql_fetch_assoc(mysql_query("SELECT url_link FROM urls WHERE url_short = '".addslashes($link)."'"));
+	$redirect = "http://".str_replace("http://","",$redirect['url_link']);
+	header('HTTP/1.1 301 Moved Permanently');  
+	header("Location: ".$redirect);  
+    }
+    
+    function shortGeneration() {
+        $short = substr(str_shuffle('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'), 0, 5);
+        return $short;
+    }
+
+    function insertNewUrl($url) {
+        mysql_query("INSERT INTO urls (url_link, url_short, url_ip, url_date) VALUES
+	(
+            '".addslashes($url)."',
+            '".$short."',
+            '".$_SERVER['REMOTE_ADDR']."',
+            '".time()."'
+	)
+        ");
+    }
+    
+    function redirectNewLink($link) {
+            $redirect = "?s=$link";
+	    return header('Location: '.$redirect); die;
+    }
+    
+    }
+?>
